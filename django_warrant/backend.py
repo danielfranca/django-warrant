f4f0db01459307ed9dff9cd7e8d798fd9b8786da
Daniel Franca <daniel.franca@protonmail.com>
2020-02-20 00:58:42 +0100

    Check if request is a valid object

--- django_warrant/backend.py
+++ django_warrant/backend.py
@@ -112,7 +112,7 @@ class CognitoBackend(AbstractCognitoBackend):
         elif email:
             user = super(CognitoBackend, self).authenticate(
                 email=email, password=password)
-        if user:
+        if user and request:
             request.session['ACCESS_TOKEN'] = user.access_token
             request.session['ID_TOKEN'] = user.id_token
             request.session['REFRESH_TOKEN'] = user.refresh_token
