df6d937bb8ab32d2bc4ff3b8127b54e939639a69
Daniel Franca <daniel.franca@protonmail.com>
2020-02-20 01:05:34 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements(filename):
     return [line for line in lineiter if line and not line.startswith("#")]
 
 
-version = '0.1.1'
+version = '0.1.2'
 
 README="""Django library that uses the warrant python utility library to provide authentication via AWS Cognito."""
 
